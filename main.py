0415956257b0251d49b7377973a9a41262edea5d
Stefano Trentin <4kill3@gmail.com>
2025-04-27 11:29:05 +0200

    Added get function to download data from the wiki

--- main.py
+++ main.py
@@ -1,6 +1,72 @@
+import urls, os, requests
+
+html_files = "./html_files/"
+
 def main():
     print("Hello from azurlane-explorer!")
+    main_menu()
+    
 
+
+def get_html_page(base_url, path):
+    existing_files = [f for f in os.listdir(html_files) if os.path.isfile(html_files + f)]
+    if (f"{path}.html") in existing_files:
+        overwrite = input(f"WARNING: {path}.html already exists. Overwrite? Y/N\n-> ")
+        if overwrite.lower() == "n":
+            return
+    res = requests.get(base_url + path)
+    with open(f"./html_files/{path}.html", "wt") as f:
+        f.write(res.text)
+
+def main_menu():
+    user_choice = input("Select mode: Get data (G) - Explore data (X) - Exit (any)\n-> ")
+    match user_choice.lower():
+        case "g":
+            _, url = list_data_sources()
+            if url == "ALL":
+                for u in urls.list_of_urls:
+                    download_data_file(u)
+            else:
+                download_data_file(url)
+        case "x":
+            _, file = list_data_files()
+            print(f"Exploring {file}")
+        case _:
+            print("exiting...")
+            return
+
+def list_data_sources():
+    print("Available data sources:")
+    for i in range(len(urls.list_of_urls)):
+        print(f"{i}: {urls.list_of_urls[i]}")
+    choice, url = get_data_source(urls.list_of_urls)
+    return choice, url
+
+def list_data_files():
+    print("Available data files:")
+    existing_files = [f for f in os.listdir(html_files) if os.path.isfile(html_files + f)]
+    for i in range(len(existing_files)):
+        print(f"{i}: {existing_files[i]}")
+    choice, file = get_data_source(existing_files)
+    return choice, file
+    
+def get_data_source(list_of_options):
+    choice = input(f"Select data source: 0..{len(list_of_options) - 1} / 666 (all)\n-> ")
+    choice = int(choice)
+    if choice >= 0 and choice < len(list_of_options):
+        return choice, list_of_options[choice]
+    if choice == 666:
+        return 0, "ALL"
+    print("ERROR: Choice out of bounds")
+    raise Exception("Invalid choice")
+
+def download_data_file(url):
+    print(f"Getting {url}")
+    try:
+        get_html_page(urls.azurlane_wiki, url)
+    except Exception as e:
+        print(f"encountered an error: {e}")
+    
 
 if __name__ == "__main__":
     main()
---
+++ urls.py
@@ -0,0 +1,44 @@
+
+azurlane_wiki               = "https://azurlane.koumakan.jp/wiki/"
+list_of_ships_with_skills   = azurlane_wiki + "List_of_Ships_with_Skills"
+list_of_ships_by_stats      = azurlane_wiki + "List_of_Ships_by_Stats"
+list_of_DD_guns             = azurlane_wiki + "List_of_Destroyer_Guns"
+list_of_CL_guns             = azurlane_wiki + "List_of_Light_Cruiser_Guns"
+list_of_CA_guns             = azurlane_wiki + "List_of_Heavy_Cruiser_Guns"
+list_of_CB_guns             = azurlane_wiki + "List_of_Large_Cruiser_Guns"
+list_of_BB_guns             = azurlane_wiki + "List_of_Battleship_Guns"
+list_of_AA_guns             = azurlane_wiki + "List_of_AA_Guns"
+list_of_AA_time_fuze_guns   = azurlane_wiki + "List_of_AA_Time_Fuze_Guns"
+list_of_torpedoes           = azurlane_wiki + "List_of_Torpedoes"
+list_of_sub_torpedoes       = azurlane_wiki + "List_of_Submarine_Torpedoes"
+list_of_fighters            = azurlane_wiki + "List_of_Fighters"
+list_of_dive_bombers        = azurlane_wiki + "List_of_Dive_Bombers"
+list_of_torpedo_bombers     = azurlane_wiki + "List_of_Torpedo_Bombers"
+list_of_seaplanes           = azurlane_wiki + "List_of_Seaplanes"
+list_of_auxiliary           = azurlane_wiki + "List_of_Auxiliary_Equipment"
+list_of_cargo               = azurlane_wiki + "List_of_Cargo"
+list_of_ASW                 = azurlane_wiki + "List_of_ASW_Equipment"
+list_of_augmentation        = azurlane_wiki + "List_of_Augment_ModulesI"
+
+list_of_urls = [
+    "List_of_Ships_by_Stats",
+    "List_of_Ships_with_Skills",
+    "List_of_AA_Guns",
+    "List_of_Battleship_Guns",
+    "List_of_Large_Cruiser_Guns",
+    "List_of_Heavy_Cruiser_Guns",
+    "List_of_Light_Cruiser_Guns",
+    "List_of_Destroyer_Guns",
+    "List_of_Torpedoes",
+    "List_of_Submarine_Torpedoes",
+    "List_of_Seaplanes",
+    "List_of_Torpedo_Bombers",
+    "List_of_Dive_Bombers",
+    "List_of_Fighters",
+    "List_of_Auxiliary_Equipment",
+    "List_of_Cargo",
+    "List_of_ASW_Equipment",
+    "List_of_AA_Time_Fuze_Guns",
+    "List_of_Augment_ModulesI",
+    
+]
